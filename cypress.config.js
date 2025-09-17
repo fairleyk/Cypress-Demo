ca7c6ceef536f7b406adaa09fc010c3e1660cf9b
fairleyk <fairleyk22@gmail.com>
2025-09-17 09:40:11 +0100

    configs

--- cypress.config.js
+++ cypress.config.js
@@ -1,1 +1,12 @@
-Demo
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://www.greggs.com",
+    env: {
+      menuUrl: "/menu",
+      supportFile: false,
+    },
+  },
+  defaultCommandTimeout: 3000,
+});
